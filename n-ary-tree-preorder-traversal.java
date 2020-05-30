1cbd57805c72777292e1a25019e24a6fc714acc5
vaibhav <vp612512@gmail.com>
2020-05-30 17:36:08 +0530

    preorder traversal

---
+++ n-ary-tree-preorder-traversal.java
@@ -0,0 +1,24 @@
+//LeetCode 589
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        LinkedList<Node> stack = new LinkedList<>();
+        LinkedList<Integer>output_arr = new LinkedList<>();
+        
+        if(root ==null){
+            return output_arr;
+        }
+        
+        stack.add(root);
+        while(!stack.isEmpty()){
+            Node node = stack.pollLast();
+            output_arr.add(node.val);
+            Collections.reverse(node.children);
+            for(Node child: node.children){
+                stack.add(child);
+            }
+        }
+        return output_arr;
+    }
+    
+}
